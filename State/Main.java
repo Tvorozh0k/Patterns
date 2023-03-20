e6983a701e1536cb131bc239772ad2f325b596b9
Oleg Moskvitin <116306584+Tvorozh0k@users.noreply.github.com>
2023-03-20 09:26:08 +0400

    Update Main.java

--- State/Main.java
+++ State/Main.java
@@ -1,24 +1,34 @@
 interface OperationState
 {
-    public int calc(int a, int b);
+    public int calc(int a, int b) throws ArithmeticException;
 }
 
 class Sum implements OperationState
 {
     @Override 
-    public int calc(int a, int b) { return a + b; }
+    public int calc(int a, int b) throws ArithmeticException { return a + b; }
 }
 
 class Subtraction implements OperationState 
 {
     @Override 
-    public int calc(int a, int b) { return a - b; }
+    public int calc(int a, int b) throws ArithmeticException { return a - b; }
 }
 
 class Multiplication implements OperationState
 {
     @Override 
-    public int calc(int a, int b) { return a * b; }
+    public int calc(int a, int b) throws ArithmeticException { return a * b; }
+}
+
+class Division implements OperationState
+{
+    @Override 
+    public int calc(int a, int b) throws ArithmeticException
+    {
+        if (b == 0) throw new ArithmeticException("На ноль делить нельзя!");
+        else return a / b;
+    }
 }
 
 class Operation
@@ -43,7 +53,7 @@ class Operation
 
 public class Main
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws ArithmeticException
 	{
 	    Operation op = new Operation(new Sum());
 	    
@@ -57,10 +67,21 @@ public class Main
 	    
 	    System.out.printf("%d * %d = %d\n", 5, 5, op.operationResult(5, 5));
 	    
+	    op.setState(new Division());
+	    
+	    System.out.printf("%d / %d = %d\n", 5, 5, op.operationResult(5, 5));
+	    System.out.printf("%d / %d = %d\n", 5, 5, op.operationResult(5, 0));
+	    
 	    /* Результат выполнения программы
 	    
 	    5 + 5 = 10
-        5 - 5 = 0
-        5 * 5 = 25 */
+            5 - 5 = 0
+            5 * 5 = 25 
+            5 / 5 = 1 
+        
+            Exception in thread "main" java.lang.ArithmeticException: На ноль делить нельзя!
+            at Division.calc(Main.java:29)
+            at Operation.operationResult(Main.java:50)
+            at Main.main(Main.java:73)                      */
 	}
 }
